945ca808ca96feab88b73d96f49ec598ff18862b
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-03-28 19:09:06 +0800

    feat: 新增測試用的 throttle 看起來 work 但是 cancel 無效，待解。

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import { EventEmitter } from './eventemitter3';
+import { throttle, debounce } from 'lodash-es';
+// console.log('throttle:', throttle);
+// console.log('debounce:', debounce);
+
 export default class ScrollTracker extends EventEmitter {
   constructor(settings) {
     super();
@@ -7,20 +11,27 @@ export default class ScrollTracker extends EventEmitter {
     };
     this.settings = Object.assign({}, defaultSettings, settings);
     console.log(this.settings);
-    this._scrollHandler = this._scrollHandler.bind(this);
+    // this._scrollHandler = this._scrollHandler.bind(this);
+    this._throttleHandler = throttle(this._scrollHandler.bind(this), 1000, {
+      leading: true,
+      trailing: false,
+    });
+    console.log(this._throttleHandler);
   }
 
   _scrollHandler() {
-    console.log('scroll ');
+    this.emit('scroll');
   }
 
   start() {
-    this.emit('START');
-    this.settings.target.addEventListener('scroll', this._scrollHandler);
+    this.emit('start');
+    // this.settings.target.addEventListener('scroll', this._scrollHandler);
+    this.settings.target.addEventListener('scroll', this._throttleHandler);
   }
 
   stop() {
-    this.emit('STOP');
-    this.settings.target.removeEventListener('scroll', this._scrollHandler);
+    this.emit('stop');
+    // this.settings.target.removeEventListener('scroll', this._scrollHandler);
+    this._throttleHandler.cancel();
   }
 }
