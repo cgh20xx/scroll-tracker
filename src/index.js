1c4daf6bc96529206964b70dab5f02fbcbc30f41
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-03-31 15:50:21 +0800

    feat: 新增 捲動方向 direction 及可偵聽原生 scroll 事件

--- src/index.js
+++ src/index.js
@@ -20,25 +20,44 @@ class ScrollTracker extends EventEmitter {
     };
     this.settings = Object.assign({}, defaultSettings, settings);
     this.target = this.settings.target;
+    this.scrollY = 0;
+    this.prevScrollY = 0;
+    this.direction = 0; // 1:向下捲 0:沒捲 -1: 向上捲
+
+    this._scrollHandler = this._scrollHandler.bind(this);
 
     this.returnFunction = modFunc[this.settings.mode](
-      this._scrollHandler.bind(this),
+      this._intervalScrollHandler.bind(this),
       this.settings.wait,
       this.settings.options
     );
   }
 
   _scrollHandler(e) {
-    console.log(Math.round(this.target.scrollY));
-    this.emit('SCROLL');
+    // console.log(e);
+    this.scrollY = Math.round(this.target.scrollY);
+    // console.log(`scrollY: ${this.scrollY}`);
+    const diffY = this.scrollY - this.prevScrollY;
+    this.direction = Math.sign(diffY);
+    // console.log('direction:', this.direction);
+    this.prevScrollY = this.scrollY;
+    this.emit('SCROLL', e);
+  }
+
+  _intervalScrollHandler(e) {
+    const currentScrollY = Math.round(this.target.scrollY);
+    console.log(currentScrollY);
+    this.emit('INTERVAL_SCROLL');
   }
 
   addEvent() {
+    this.target.addEventListener('scroll', this._scrollHandler);
     this.target.addEventListener('scroll', this.returnFunction);
     this.emit('ADD_EVENT');
   }
 
   removeEvent() {
+    this.target.removeEventListener('scroll', this._scrollHandler);
     this.target.removeEventListener('scroll', this.returnFunction);
     this.emit('REMOVE_EVENT');
   }
