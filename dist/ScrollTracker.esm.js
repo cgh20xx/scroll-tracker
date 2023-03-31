f292277a8dad93adcf0f0eeaed3f89156637908e
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2023-03-31 19:08:23 +0800

    feat: 新增 scrollY scrollRatio 可追蹤捲軸變化。目前初版完成。

--- dist/ScrollTracker.esm.js
+++ dist/ScrollTracker.esm.js
@@ -943,9 +943,10 @@ class ScrollTracker extends EventEmitter {
 
     this.settings = Object.assign({}, defaultSettings, settings);
     this.target = this.settings.target;
-    this.scrollY = 0;
-    this.prevScrollY = 0;
-    this.direction = 0; // 1:向下捲 0:沒捲 -1: 向上捲
+    this._scrollY = 0;
+    this._prevScrollY = 0;
+    this._deltaY = 0;
+    this._direction = 0; // 1:向下捲 0:沒捲 -1: 向上捲
 
     this._scrollHandler = this._scrollHandler.bind(this);
 
@@ -956,14 +957,41 @@ class ScrollTracker extends EventEmitter {
     );
   }
 
+  get scrollY() {
+    return this._scrollY;
+  }
+
+  get deltaY() {
+    return this._deltaY;
+  }
+
+  get direction() {
+    return Math.sign(this._deltaY);
+  }
+
+  // scrollHeight: 可捲動的高度
+  get scrollHeight() {
+    return this.target.document.body.offsetHeight - this.target.innerHeight;
+  }
+
+  // scrollBarHeight: 捲軸bar的高度
+  get scrollBarHeight() {
+    return (
+      this.target.innerHeight *
+      (this.target.innerHeight / this.target.document.body.offsetHeight)
+    );
+  }
+
+  get scrollRatio() {
+    return this.scrollY / this.scrollHeight;
+  }
+
   _scrollHandler(e) {
     // console.log(e);
-    this.scrollY = Math.round(this.target.scrollY);
-    // console.log(`scrollY: ${this.scrollY}`);
-    this.deltaY = this.scrollY - this.prevScrollY;
-    this.direction = Math.sign(this.deltaY);
-    // console.log('direction:', this.direction);
-    this.prevScrollY = this.scrollY;
+    this._scrollY = Math.round(this.target.scrollY);
+    // console.log(`scrollY: ${this._scrollY}`);
+    this._deltaY = this._scrollY - this._prevScrollY;
+    this._prevScrollY = this._scrollY;
     this.emit('SCROLL', e);
   }
 
